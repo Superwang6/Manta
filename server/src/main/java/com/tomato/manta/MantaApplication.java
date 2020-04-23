1ec02113131e35ebc70e17098532741ae7b87736
Mr.Wang <52198480+Superwang6@users.noreply.github.com>
2020-04-23 16:39:03 +0800

    项目构建

---
+++ server/src/main/java/com/tomato/manta/MantaApplication.java
@@ -0,0 +1,13 @@
+package com.tomato.manta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MantaApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/tomato/manta/MantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomato.manta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
