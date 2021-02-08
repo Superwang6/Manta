5cefc043d167628faca884dc826df07c9ec8d439
Mr.Wang <52198480+Superwang6@users.noreply.github.com>
2021-02-08 16:13:44 +0800

    继续写web

---
+++ web/src/components/GoodsManager.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>商铺管理</h1>
+</template>
+
+<script>
+    export default {
+        name: "GoodsManager"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,10 +1,30 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import Login from '../components/login';
+import Main from '../components/main';
+import GoodsManager from '../components/GoodsManager'
+
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
-
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: Main,
+      children :[
+        {
+          path: '/goodsManager',
+          component: GoodsManager
+        }
+      ]
+    }
   ]
 })
