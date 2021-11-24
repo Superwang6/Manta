6d9234423da4652bb0f5123adf72cc3e1898dd20
Superwang6 <1075227991@qq.com>
2021-11-24 16:40:07 +0800

    新增web项目,版本 vue3.2

---
+++ web/src/base/frame/app/Application.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-container class="content">
+    <el-header> <Header></Header> </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang='ts' setup>
+import Header from "@/base/frame/app/header/Header.vue";
+</script>
+<style scoped>
+.content {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ web/src/base/frame/app/header/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>hahah</div>
+</template>
+<script lang='ts' setup>
+</script>
+<style scoped>
+</style>
